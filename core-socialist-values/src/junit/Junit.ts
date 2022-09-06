02703afc729ce07f0df86d81817990e2c96b3843
duanluan <duanluan@outlook.com>
2022-09-06 16:46:07 +0800

    style: 删除无用变量

--- core-socialist-values/src/junit/Junit.ts
+++ core-socialist-values/src/junit/Junit.ts
@@ -1,11 +1,9 @@
 export default class Junit {
 
   private static selectors = {
-    reactBar: '.css-lpiycv',
     junitAbout: '#about',
     junitLogo: 'img[src="/junit5/assets/img/junit5-logo.png"]',
     junitFavicon: 'link[rel="icon"]',
-    codeceptBar: '.sub-bar .message',
   }
 
   private static junitLogoUrl = 'https://raw.iqiq.io/duanluan/tampermonkey-scripts/main/core-socialist-values/src/junit/imgs/junit5-logo.png'
